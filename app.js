38d5264c6ccbd0a0158e0103538822d5cf0a26ee
Ivan Perić <ivan.peric@intra.eronet.ba>
2019-03-21 15:03:56 +0100

    added functionality

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 let btn = document.querySelector('.btn');
 let city = document.getElementById('city');
+let results = document.querySelector('.results');
     
-
-
 
 btn.addEventListener('click', () => {
     let inp = city.value;
-    console.log(city.value)
+    
     let proxy = "https://cors-anywhere.herokuapp.com/";
     let api = `${proxy}api.openweathermap.org/data/2.5/weather?q=${inp}&units=metric&APPID=30955fcd97f29207918131713de09b85`;
-    // fetch(api).then((response => {
-    //     console.log('ran');
-    //     console.log(response.json());
-    // }))
     fetch(api).then((response) => {
-        console.log(response.json());
+        return response.json();
+    }).then((data) => {
+        let city = document.createElement('H1');
+        let temp = document.createElement('DIV');
+        let wind = document.createElement('DIV');
+        let press = document.createElement('DIV');
+
+        city.innerHTML = data.name;
+        temp.innerHTML = `Temperature: ${Math.round(data.main.temp)}°C`;
+        wind.innerHTML = `Wind: ${data.wind.speed} km/h`;
+        press.innerHTML = `Pressure: ${data.main.pressure} hPa`;
+        results.innerHTML = '';
+        results.appendChild(city);
+        results.appendChild(temp);
+        results.appendChild(wind);
+        results.appendChild(press);
+        console.log(data);
     })
 })
